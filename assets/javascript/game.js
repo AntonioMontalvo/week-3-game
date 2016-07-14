32ad2472dab77c78cca247a7f618bcaec59d9e57
Antonio Montalvo <montalvocode@yahoo.com>
2016-07-14 12:28:19 -0700

    tested files and array artists

---
+++ assets/javascript/game.js
@@ -0,0 +1,2 @@
+//create an array for the artists
+var artists = ["Miles Davis", "Chick Corea", "Herbie Hancock", "Nina Simone", "Stan Getz", "Sarah Vaughan", "Duke Ellington", "Billie Holiday", "John Coltrane", "Charli Parker", "Louis Armstrong"];
