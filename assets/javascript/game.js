edeb681d712596dbd78daa8fbebd059728b53537
Antonio Montalvo <montalvocode@yahoo.com>
2016-07-16 07:02:53 -0700

    add getArtit(), improve artists

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,46 @@
 //create an array for the artists
-var artists = ["Miles Davis", "Chick Corea", "Herbie Hancock", "Nina Simone", "Stan Getz", "Sarah Vaughan", "Duke Ellington", "Billie Holiday", "John Coltrane", "Charli Parker", "Louis Armstrong"];
+var artists = [
+  {firstname:"Miles" ,
+   lastname:"Davis"
+  },
+  {firstname:"Chick Corea",
+  lastname:"Davis"
+  },
+  {firstname:"Herbie Hancock",
+  lastname:"Davis"
+  },
+  {firstname:"Nina",
+  lastname:"Simone"
+  },
+  {firstname:"Stan",
+  lastname:"Getz"
+  },
+  {firstname:"Sarah",
+  lastname:"Vaughan"
+  },
+  {firstname:"Louis",
+  lastname:"Armstrong"
+  },
+  {firstname:"Duke",
+  lastname:"Ellington"
+  },
+  {firstname:"John",
+  lastname:"Coltrane"
+  },
+  {firstname:"Charlie",
+  lastname:"Parker"
+  }
+];
+
+function getArtist(){
+  console.log(artists[0].firstname + " "+ artists[0].lastname);
+  selected = artists[0].firstname + " "+ artists[0].lastname;
+  return selected;
+}
+
+var guessArtist = {
+  dashes:[],
+//
+
+
+}
